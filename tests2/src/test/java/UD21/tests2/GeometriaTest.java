044bdc3bd56c1ea384c3eea4a27f7e58dc3547e0
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-17 12:03:45 +0100

    commitFinal

--- tests2/src/test/java/UD21/tests2/GeometriaTest.java
+++ tests2/src/test/java/UD21/tests2/GeometriaTest.java
@@ -27,6 +27,20 @@ class GeometriaTest {
     }
  
     @Test
+    public void testGeometriaPorDefecto()
+    {
+        System.out.println("testGeometriaPorDefecto()");
+        Geometria geo2 = new Geometria();
+        int esper1 = 9;
+        String esper2 = "Default";
+        int res1 = geo2.getId();
+        String res2 = geo2.getNom();
+
+        assertEquals(esper1, res1);
+        assertEquals(esper2, res2);
+    }
+    
+    @Test
     public void testAreaCuadrado()
     {
         System.out.println("testAreaCuadrado()");
@@ -62,6 +76,96 @@ class GeometriaTest {
         System.out.println("testAreaRectangulo()");
         int result = geo.arearectangulo(2, 2);
         double esper = 4;
+
+        assertEquals(esper, result);
+    }
+    
+    @Test
+    public void testAreaPentagono()
+    {
+        System.out.println("testAreaPentagono()");
+        int result = geo.areapentagono(5, 2);
+        double esper = 5;
+
+        assertEquals(esper, result);
+    }
+    
+    @Test
+    public void testAreaRombo()
+    {
+        System.out.println("testAreaRombo()");
+        int result = geo.arearombo(5, 2);
+        double esper = 5;
+
+        assertEquals(esper, result);
+    }
+    
+    @Test
+    public void testAreaRomboide()
+    {
+        System.out.println("testAreaRomboide()");
+        int result = geo.arearomboide(5, 2);
+        double esper = 10;
+
+        assertEquals(esper, result);
+    }
+    
+    @Test
+    public void testAreaTrapecio()
+    {
+        System.out.println("testAreaTrapecio()");
+        int result = geo.areatrapecio(5, 5, 5);
+        double esper = 25;
+
+        assertEquals(esper, result);
+    }
+    
+    @Test
+    public void testFigura()
+    {
+        System.out.println("testFigura()");
+        String result = geo.figura(2);
+        String esper = "Circulo";
+
+        assertEquals(esper, result);
+    }
+    
+    @Test
+    public void testSetGetId()
+    {
+        System.out.println("testSetGetId()");
+        geo.setId(1);;
+        int esper = geo.getId();
+
+        assertEquals(esper, 1);
+    }
+    
+    @Test
+    public void testSetGetNom()
+    {
+        System.out.println("testSetGetNom()");
+        geo.setNom("Miquel Angel");;
+        String esper = geo.getNom();
+
+        assertEquals(esper, "Miquel Angel");
+    }
+    
+    @Test
+    public void testSetGetArea()
+    {
+        System.out.println("testSetGetArea()");
+        geo.setArea(12);;
+        double esper = geo.getArea();
+
+        assertEquals(esper, 12);
+    }
+    
+    @Test
+    public void testToString()
+    {
+        System.out.println("testToString()");
+        String result = geo.toString();
+        String esper = "Geometria [id=" + geo.getId() + ", nom=" + geo.getNom() + ", area=" + geo.getArea() + "]";
 
         assertEquals(esper, result);
     }
